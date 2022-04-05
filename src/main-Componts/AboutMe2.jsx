5e96fc88d01ba92d65e4e773181500489f6dcf95
aldairandre <aldairandre99@gmail.com>
2022-04-05 12:40:12 +0000

    por terminar

--- src/main-Componts/AboutMe2.jsx
+++ src/main-Componts/AboutMe2.jsx
@@ -1,6 +1,4 @@
-import "./css/style.css";
-import "./css/flexbox.css";
-import "./css/mediaquery.css";
+import "./css/AboutMe2-style.css";
 
 export default function AboutMe2(){
     return(
@@ -10,12 +8,8 @@ export default function AboutMe2(){
                 <p className="aboutMe-paragraph">
                 Olá, sou o Aldair Dos Santos André, apaixonado por Tecnologia,
                 Design, Musica, Artes.        
-                </p>  
-                <p className="aboutMe-paragraph">
                 Extremamente
                 motivado por desafios e sempre aprendendo durante minha jornada como desenvolvedor Front-End.
-                </p>  
-                <p className="aboutMe-paragraph">
                 Atualmente
                 venho desenvolvendo minhas habilidades nas tecnologias e linguagens como
                 JavaScript, React.js, Figma, UI/UX, PHP, Nodes, SQL, MYSQL, MongoBD.
