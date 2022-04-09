0e72f226919c7575ed7bd16d42897c3b17373a1c
Aldair André <aldairandre99@gmail.com>
2022-04-09 12:44:37 +0100

    fixbug do menu

--- src/header.jsx
+++ src/header.jsx
@@ -1,18 +1,22 @@
 import './header-style.css';
 
+const activeMenu = () => document.getElementById('ul-navList').classList.add('active')
+const removeMenu = () => document.getElementById('ul-navList').classList.remove('active')
+
 
 function Header(){
     return(
         <header>
             <h1>Portfólio</h1>
             <nav className="container-nav">
-                <ul>
+                <ul id='ul-navList'>
                     <li className="nav-items"><a href="#aboutMe">Sobre mim</a></li>
                     <li className="nav-items"><a href="#projects">Projetos</a></li>
                     <li className="nav-items"><a href="#services">Serviços</a></li>
                     <li className="nav-items"><a href="#skills">Minhas Skills</a></li>
+                    <li><i className='remove-menu-btn' onClick={removeMenu}>X</i></li>
                 </ul>
-                <i className="bi bi-list menu-icon" id="menu-icon-btn"></i>
+                <i className="bi bi-list menu-icon" id="menu-icon-btn" onClick={activeMenu}></i>
             </nav>
         </header>
     )
