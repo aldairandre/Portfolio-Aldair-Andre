0a330075a332c62b2806000cc9f802098e8b0b67
Aldair André <aldairandre99@gmail.com>
2022-05-14 12:49:17 +0100

    refatorando com styled componentes

---
+++ src/Components/footer/footer.jsx
@@ -0,0 +1,8 @@
+import {StyleFooter,StyleP,StyleSpan} from './stylefooter'
+export default function Footer(){
+    return(
+        <StyleFooter>
+            <StyleP className="footer-paragraph">Desenvolvido por <StyleSpan>Aldair André</StyleSpan></StyleP>
+        </StyleFooter>
+    )
+}
---
+++ src/Components/footer/stylefooter.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+const StyleFooter = styled.footer`
+   width: 100%;
+   height: 60px;
+   background: #212121;
+   display: flex;
+   align-items: center;
+   justify-content: center;
+`
+ 
+const StyleP = styled.p`
+   font-style: normal;
+   font-weight: normal;
+   font-size: 16px;
+   line-height: 21px;
+   color: #F9F9F9;
+   background: #212121;
+`
+
+
+const StyleSpan = styled.span`
+   color: #323D70;
+   background-color: #212121;
+`
+
+export {StyleFooter,StyleP,StyleSpan}
