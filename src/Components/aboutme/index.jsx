16bde569b3cef2e573a56af1265393f394a21ce9
Aldair André <aldairandre99@gmail.com>
2022-05-14 14:10:21 +0100

    refatorando usando styled components

---
+++ src/Components/aboutme/index.jsx
@@ -0,0 +1,19 @@
+import {StyleSection,StyleArticle,StyleTitle,StyleButton,StyleDownButton,StyleDownA,StyledContactButton} from "./styleIndex";
+
+
+export default function AboutMe(){
+    return(  
+        <StyleSection>
+            <StyleArticle>
+                <StyleTitle className="titles">Olá, eu sou o Aldair André</StyleTitle>
+                <p className="apresentation-paragraph">Desenvolvedor Front-End</p>
+                <StyleButton className="button" id="download-button">
+                <StyleDownButton className="link-download" href="./assets/Lucas Santos Rodrigues.pdf">Download CV</StyleDownButton>
+                </StyleButton>
+                <StyledContactButton className="button" id="contact-button">
+                <StyleDownA className="link-whatsapp" href="" target="_blank" >Entrar em contato</StyleDownA>
+                </StyledContactButton>
+            </StyleArticle>
+        </StyleSection>
+    )
+}
